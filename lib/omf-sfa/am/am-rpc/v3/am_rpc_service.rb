cd13c1b00c31e9f191242943a8c747c836d08459
Marios Avgeris <avgeris.marios@gmail.com>
2018-12-13 16:08:46 +0000

    add missing attribute to translated rspec (debug @)

--- lib/omf-sfa/am/am-rpc/v3/am_rpc_service.rb
+++ lib/omf-sfa/am/am-rpc/v3/am_rpc_service.rb
@@ -205,7 +205,7 @@ module OMF::SFA::AM::RPC::V3
       debug " translated "
       new_result = Nokogiri::XML(result)
       # new_result.at('node').add_child("<ns3:lease_ref id_ref=" + new_result.at('node').next.next.attributes['id'].value + "/>")
-      lease_ref = Nokogiri::XML::Node.new("lease_ref", doc)
+      lease_ref = Nokogiri::XML::Node.new("lease_ref", new_result)
       lease_ref["id_ref"] = new_result.children.first.children[3].attributes['id'].value
       lease_ref.namespace = new_result.root.namespace_definitions.find{|ns| ns.prefix=="ns3"}
       new_result.children.first.children[1].add_child(lease_ref)
@@ -225,7 +225,7 @@ module OMF::SFA::AM::RPC::V3
       result.sub! 'sliver_id', 'component_id'
       result.sub! 'leaseID', 'id'
       new_result = Nokogiri::XML(result)
-      lease_ref = Nokogiri::XML::Node.new("lease_ref", doc)
+      lease_ref = Nokogiri::XML::Node.new("lease_ref", new_result)
       lease_ref["id_ref"] = new_result.children.first.children[3].attributes['id'].value
       lease_ref.namespace = new_result.root.namespace_definitions.find{|ns| ns.prefix=="ns3"}
       new_result.children.first.children[1].add_child(lease_ref)
