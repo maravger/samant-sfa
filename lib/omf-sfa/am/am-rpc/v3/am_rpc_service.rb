3b2897a42ed3dda28cae457c71a43520cf31a3b6
Marios Avgeris <avgeris.marios@gmail.com>
2019-03-07 16:41:28 +0200

    add debug messages

--- lib/omf-sfa/am/am-rpc/v3/am_rpc_service.rb
+++ lib/omf-sfa/am/am-rpc/v3/am_rpc_service.rb
@@ -204,7 +204,7 @@ module OMF::SFA::AM::RPC::V3
       result = `java -jar ./lib/omf-sfa/am/am-rpc/omn_translator/omnlib-jar-with-dependencies.jar -o advertisement -i #{filename}`
       debug " translated " + result.inspect
       new_result = Nokogiri::XML(result)
-      debug " new result " + new_result.inspect
+      debug " new result " + new_result.to_xml
       # new_result.at('node').add_child("<ns3:lease_ref id_ref=" + new_result.at('node').next.next.attributes['id'].value + "/>")
       lease_ref = Nokogiri::XML::Node.new("lease_ref", new_result)
       debug " lease_ref " + lease_ref.inspect
